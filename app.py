acdbf651cdad077e1042065c084adcff1a922570
Dr-Hutchinson <45830997+Dr-Hutchinson@users.noreply.github.com>
2022-06-25 07:43:18 -0400

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ with st.sidebar:
     """
     <style>
     [data-testid="stSidebar"][aria-expanded="true"] > div:first-child {
-        width: 425px;
+        width: 450px;
     }
     [data-testid="stSidebar"][aria-expanded="false"] > div:first-child {
         width: 500px;
