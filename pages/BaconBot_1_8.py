ec3a30b67465d0b7d2d5cb46994f48aba167b224
Dr-Hutchinson <45830997+Dr-Hutchinson@users.noreply.github.com>
2022-06-25 13:16:23 -0400

    Update BaconBot_1_8.py

--- pages/BaconBot_1_8.py
+++ pages/BaconBot_1_8.py
@@ -55,19 +55,24 @@ def app():
             #with st.expander("Advanced Settings:"):
                 #prompt_booster = st.radio("Zero Shot vs. Few Shot Prompting. If you chose one of the prompt boosters below, the AI model will be given pre-selected examples of the type of prompt you want to submit, increasing the chance of a better reply. However, this will also increase the chance the reply will repeat the booster choice. Choose 'None' to field questions without a booster.", ["None", "Question Booster", "Rationale Booster", "Haiku Booster"])
 
-            with st.container("Question Bank:"):
+            option = st.selectbox("Question Bank:", ("Biographical, Philosophy of Science")):
 
+            if option == "Biographical":
                 with st.expander("Biographical Questions:"):
                     bio_question = st.radio("Questions concerning Bacon's life and career.", ["Describe your early life and education.", "How would you describe your career in government?", "What contributions did you make in the field of science?"])
+            else:
+                with st.expander("Philosophy of Science Questions:")
+                    science_question = st.radio("Questions concerning Bacon's Philosophy of Science.", ["option 1", "option 2"])
 
-                submit_button_1 = st.form_submit_button(label='Submit Question')
+
+            submit_button_1 = st.form_submit_button(label='Submit Question')
                 #with st.expander("Test:"):
                     #test = st.radio("Test",["test1", "test2"])
 
             #temperature_dial = st.slider("Temperature Dial. Lower values are generally more accurate, but lower extremes yield more repetitive replies. Higher values are more creative, but higher extremes result in incoherent responses.", 0.0, 1.0)
             #response_length = st.slider("Response Length. Recommended range is 75-150 for general questions, 150-250 for rationale questions, and 25-50 for haikus.", 25, 250)
             #submission_text = st.text_area("Enter your questions and comments below to Francis Bacon in this space. Be patient as he considers your statement.", max_chars=1000)
-            #submit_button_1 = st.form_submit_button(label='Submit Question')
+            #j2submit_button_1 = st.form_submit_button(label='Submit Question')
 
             if submit_button_1:
                 os.environ["OPENAI_API_KEY"] = st.secrets["openai_api_key"]
