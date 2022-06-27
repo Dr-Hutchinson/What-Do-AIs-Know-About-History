02b078490249e5478ffcdf7b10a46e55aff88dda
Dr-Hutchinson <45830997+Dr-Hutchinson@users.noreply.github.com>
2022-06-27 11:35:47 -0400

    Update primary_source_analyzer.py

--- pages/primary_source_analyzer.py
+++ pages/primary_source_analyzer.py
@@ -75,7 +75,7 @@ def app():
                             user="0",
                             max_tokens=response_length,
                             frequency_penalty=0.35,
-                            presence_penalty=0.25)                           )
+                            presence_penalty=0.25)                           
 
                         response_json = len(summon["choices"])
 
