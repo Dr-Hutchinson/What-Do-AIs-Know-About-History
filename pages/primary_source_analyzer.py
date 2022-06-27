3ae35861c545c3fef4321cfed3c40174759984dd
Dr-Hutchinson <45830997+Dr-Hutchinson@users.noreply.github.com>
2022-06-27 12:54:18 -0400

    Update primary_source_analyzer.py

--- pages/primary_source_analyzer.py
+++ pages/primary_source_analyzer.py
@@ -140,7 +140,7 @@ def app():
 
                                 # filter or display OpenAI outputs, record outputs to Google Sheets API
                         if int(filter_function()) < 2:
-                            st.write("GPT's Analysis:")
+                            st.write("GPT-3's Analysis:")
                             st.write(output)
                             #st.write("\n\n\n\n")
                             #st.subheader('As Lord Bacon says, "Truth will sooner come out from error than from confusion."  Please click on the Rank Bacon button above to rank this reply for future improvement.')
