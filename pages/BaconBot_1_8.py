04a7bddd57d0471b419e1f4ec7801e5230c1c555
Dr-Hutchinson <45830997+Dr-Hutchinson@users.noreply.github.com>
2022-06-27 16:03:02 -0400

    Update BaconBot_1_8.py

--- pages/BaconBot_1_8.py
+++ pages/BaconBot_1_8.py
@@ -48,7 +48,7 @@ def app():
                     #with st.expander("Advanced Settings:"):
                         #prompt_booster = st.radio("Zero Shot vs. Few Shot Prompting. If you chose one of the prompt boosters below, the AI model will be given pre-selected examples of the type of prompt you want to submit, increasing the chance of a better reply. However, this will also increase the chance the reply will repeat the booster choice. Choose 'None' to field questions without a booster.", ["None", "Question Booster", "Rationale Booster", "Haiku Booster"])
 
-                    question = st.radio("Questions concerning Bacon's life and career.", ["Describe your youth and family.", "How did your education shape your later career?", "How would you describe your career in government?", "Can you elaborate on the circumstances on your death?"])
+                    question = st.radio("Questions concerning Bacon's life and career.", ["Describe your youth and family.", "How did your education shape your later career?", "How would you describe your career in government?"])
 
                     submit_button_1 = st.form_submit_button(label='Submit Question')
                         #with st.expander("Test:"):
