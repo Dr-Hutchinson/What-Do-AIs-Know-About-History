e00903d8c4280042ef025c3a31de471db5e9835a
Dr-Hutchinson <45830997+Dr-Hutchinson@users.noreply.github.com>
2022-07-01 14:44:54 -0400

    Update benchmarks_results.py

--- pages/benchmarks_results.py
+++ pages/benchmarks_results.py
@@ -124,7 +124,7 @@ def app():
         with st.form('form1'):
             st.subheader("Human Interface:")
 
-            st.write('Read the question below, and select the correct answer. Click on the "Click here to submit your answer" button to see the correct answer.')
+            st.write('Read the question below, and select the best answer. Click on the "Click here to submit your answer" button to see if you answered correctly.')
 
             st.write("Question #" + st.session_state.question_number1 + ":" + "\n\n" + st.session_state.question)
             submit_answer = st.radio("Choose from the following options:", ["A: " + st.session_state.option_a, "B: " + st.session_state.option_b, "C: " + st.session_state.option_c, "D: " + st.session_state.option_d])
