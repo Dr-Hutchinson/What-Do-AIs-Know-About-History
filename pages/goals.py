2bfcaba5d5104e654b35c9ef8cf0b31c47e47f60
Dr-Hutchinson <45830997+Dr-Hutchinson@users.noreply.github.com>
2022-11-22 23:31:32 -0500

    Update goals.py

--- pages/goals.py
+++ pages/goals.py
@@ -2,6 +2,23 @@ import streamlit as st
 
 def app():
 
+    col1, col2, col3, col4, col5 = st.columns(5)
+
+    with col1:
+        st.write(' ')
+
+    with col2:
+        st.image('./logo.png', width=600, caption="Designed with Midjourney")
+
+    with col3:
+        st.write(' ')
+
+    with col4:
+        st.write(' ')
+
+    with col5:
+        st.write(' ')
+
     st.header("Project Goals:")
 
     st.write("This digital history experiment explores the potential of these technologies for academic research and as a force for disinformation. The project uses OpenAI's [GPT-3](https://en.wikipedia.org/wiki/GPT-3) model as a case study due to its accessibility and position as the subject of robust scientific study and debate.")
