bbc23cbcafd32345a4902fa53cb42dd61a5e61c5
Dr-Hutchinson <45830997+Dr-Hutchinson@users.noreply.github.com>
2022-11-28 17:46:09 -0500

    Update goals.py

--- pages/goals.py
+++ pages/goals.py
@@ -21,7 +21,7 @@ def app():
 
     st.header("Project Goals:")
 
-    st.write("This digital history experiment explores the potential of these technologies for academic research and as a force for disinformation. The project uses OpenAI's [GPT-3](https://en.wikipedia.org/wiki/GPT-3) model as a case study due to its accessibility and position as the subject of robust scientific study and debate.")
+    st.write("This digital history experiment explores the potential of AI applications for both academic research and as a force for disinformation. The project uses OpenAI's [GPT-3](https://en.wikipedia.org/wiki/GPT-3) model as a case study due to its accessibility and position as the subject of robust scientific study and debate.")
     st.write("In May 2022, I presented the initial findings from this project to the [Digital History Working Group for the Consortium For History of Science, Technology, And Medicine](https://www.chstm.org/content/digital-history). I expand on these findings in an academic paper currently under peer review.")
 
     st.header("Project Methods:")
