33246ca8f67ca360b52f1e934e01a4fa792a30c7
Dr-Hutchinson <45830997+Dr-Hutchinson@users.noreply.github.com>
2022-11-28 22:22:26 -0500

    Update primary_source_analyzer.py

--- pages/primary_source_analyzer.py
+++ pages/primary_source_analyzer.py
@@ -390,8 +390,6 @@ def app():
                         os.environ["OPENAI_API_KEY"] = st.secrets["openai_api_key"]
                         now = dt.now()
 
-                        #primary_source_analysis = prompt + "\n" + historical_method + histriography_options + "\n\n" + instructions + k_shot + "/nLet's try another." + "/nPrimary Source: " + "\n" + household_text + "\n" + source_information + "\n" + instructions + "\n"
-
                         primary_source_analysis = prompt + "\n" + historical_method + "\n\n" + histriography_options + "\n\n" + instructions + k_shot + "/nLet's try another." + "/nPrimary Source: " + "\n" + zexu_letter + "\n" + source_information + "\n" + instructions
 
                         os.environ["OPENAI_API_KEY"] = st.secrets["openai_api_key"]
@@ -471,7 +469,7 @@ def app():
 
                                 # filter or display OpenAI outputs, record outputs to Google Sheets API
                         if int(filter_function()) < 2:
-                            st.write("GPT-3's Analysis:")
+                            st.header("GPT-3's Analysis:")
                             st.write(output)
                             #st.write("\n\n\n\n")
                             #st.subheader('As Lord Bacon says, "Truth will sooner come out from error than from confusion."  Please click on the Rank Bacon button above to rank this reply for future improvement.')
@@ -481,7 +479,7 @@ def app():
                         st.header("Here is the prompt fed to GPT-3 for analyzing these sources:")
                         st.write(prompt)
                         st.write(historical_method + histriography_options)
-                        st.write(instructions)                            
+                        st.write(instructions)
                         st.write("\n\n\n\n")
                         st.write("OpenAI's Content Filter Ranking: " +  output_label)
 
@@ -533,12 +531,12 @@ def app():
 
                     st.header('Primary Source - Mary Lease, "Women in the Farmers Alliance" (1891)')
 
-                    hayseed_lyrics = '"Madame President and Fellow Citizens:— If God were to give me my choice to live in any age of the world that has flown, or in any age of the world yet to be, I would say, O God, let me live here and now, in this day and age of the world’s history. We are living in a grand and wonderful time: we are living in a day when old ideas, old traditions, and old customs have broken loose from their moorings, and are hopelessly adrift on the great shoreless, boundless sea of human thought; we are living in a time when the gray old world begins to dimly comprehend that there is no difference between the brain of an intelligent woman and the brain of an intelligent man; no difference between the soul-power or brain power that nerved the arm of Charlotte Corday to deeds of heroism, and which swayed old John Brown behind his barricade at Ossawattomie; we are living in a day and age when the women of industrial societies and the Alliance women have become a mighty factor in the politics of this nation; when the mighty dynamite of thought is stirring the hearts of men of this world from centre to circumference, and social and political structure and stirring the hearts of men from centre to circumference, and this thought is crystallizing into action.'
+                    lease_speech = '"Madame President and Fellow Citizens:— If God were to give me my choice to live in any age of the world that has flown, or in any age of the world yet to be, I would say, O God, let me live here and now, in this day and age of the world’s history. We are living in a grand and wonderful time: we are living in a day when old ideas, old traditions, and old customs have broken loose from their moorings, and are hopelessly adrift on the great shoreless, boundless sea of human thought; we are living in a time when the gray old world begins to dimly comprehend that there is no difference between the brain of an intelligent woman and the brain of an intelligent man; no difference between the soul-power or brain power that nerved the arm of Charlotte Corday to deeds of heroism, and which swayed old John Brown behind his barricade at Ossawattomie; we are living in a day and age when the women of industrial societies and the Alliance women have become a mighty factor in the politics of this nation; when the mighty dynamite of thought is stirring the hearts of men of this world from centre to circumference, and social and political structure and stirring the hearts of men from centre to circumference, and this thought is crystallizing into action.'
                     source_information = 'Source Information: The Primary Source is a speech entitled "Women in the Farmers Alliance," given by Mary Lease to the National Council of Women National Meeting in Washington D.C. in 1891.'
 
                     st.image(image='./lease.png')
                     st.write("Source: National Council of Women of the United States, _Transactions of the National Council of Women of the United States: Assembled in Washington, D.C., February 22 to 25, 1891_, 214. Avaliable via [Google Books.](https://books.google.com/books?id=bpU0xGnVETsC&newbks=1&newbks_redir=0&dq=If%20God%20were%20to%20give%20me%20my%20choice%20to%20live%20in%20any%20age%20of%20the%20world%20that%20has%20flown%2C%20or%20in%20any%20age%20of%20the%20world%20yet%20to%20be%2C%20I%20would%20say%2C%20O%20God%2C%20let%20me%20live%20here%20and%20now%2C%20in%20this%20day%20and%20age%20of%20the%20world%E2%80%99s%20history.&pg=PA214#v=onepage&q&f=false)")
-                    st.write(hayseed_lyrics)
+                    st.write(lease_speech)
                     st.write(source_information)
 
                     submit_button_1 = st.form_submit_button(label='Analyze Source')
@@ -547,10 +545,14 @@ def app():
 
                     if submit_button_1:
 
+                        k_shot = "Step 1 - Contextualization: The Primary Source is an American political campaign song popularized in 1890, and published by a Nebraska newspaper known as the Farmer's Alliance. The song reflects the historical period of America's Gilded Age, a time of great economic growth and prosperity. However, this prosperity was not evenly distributed, and many Americans were left behind. The song speaks to this inequality, with the 'hayseed' protagonist being oppressed by wealthy interests. This source provides insights into the larger historic events of the Gilded Age, including the rise of monopolies and the power of political bosses. It also offers insight into the ideologies of the time, including populism and progressivism.  \n\nStep 2 - Purpose: The purpose of the Primary Source is to offer a populist critique of the Gilded Age status quo. The song argues that the rich are oppressing the poor, and that this needs to change. It calls for a return to more egalitarian values, and for Americans to stand up against the powerful interests that are keeping them down.  \n\nStep 3 - Audience: The intended audience of the Primary Source is working-class Americans who feel left behind by the country's economic success. The song speaks to their situation, and offers a message of hope that things can change. It is also meant to inspire them to take action against the wealthy interests that are oppressing them.  \n\nStep 4 - Historiographical Interpretation: Different historians would interpret this source differently, depending on their historiographical school of thought. For example, Marxist historians would focus on the class conflict inherent in the song, and see it as reflective of the wider struggle between workers and capitalists during the Gilded Age. Postcolonial historians might focus on the hayseed protagonist's position as an outsider within American society, and use the song to explore issues of race and ethnicity during the period. Gender historians might focus on the fact that the hayseed is male, and use the song to explore issues of masculinity during the Gilded Age."
+
                         os.environ["OPENAI_API_KEY"] = st.secrets["openai_api_key"]
                         now = dt.now()
 
-                        primary_source_analysis = prompt + "\n" + historical_method + histriography_options + "\n\n" + "Primary Source: " + "\n" + hayseed_lyrics + "\n" + source_information + "\n" + instructions + "\n"
+                        #primary_source_analysis = prompt + "\n" + historical_method + histriography_options + "\n\n" + instructions + k_shot + "/nLet's try another." + "/nPrimary Source: " + "\n" + household_text + "\n" + source_information + "\n" + instructions + "\n"
+
+                        primary_source_analysis = prompt + "\n" + historical_method + histriography_options + "\n\n" + instructions + k_shot + "\nLet's try another." + \n"Primary Source: " + "\n" + lease_speech + "\n" + source_information + "\n" + instructions + "\n"
 
                         response_length = 1500
 
@@ -626,7 +628,7 @@ def app():
 
                                 # filter or display OpenAI outputs, record outputs to Google Sheets API
                         if int(filter_function()) < 2:
-                            st.write("GPT-3's Analysis:")
+                            st.header("GPT-3's Analysis:")
                             st.write(output)
                             #st.write("\n\n\n\n")
                             #st.subheader('As Lord Bacon says, "Truth will sooner come out from error than from confusion."  Please click on the Rank Bacon button above to rank this reply for future improvement.')
