7ad022eae8c6a4e8d9245b0dee986836a6314c2a
Dr-Hutchinson <45830997+Dr-Hutchinson@users.noreply.github.com>
2024-07-13 13:38:19 -0400

    Update intro.py

--- pages/intro.py
+++ pages/intro.py
@@ -35,4 +35,4 @@ def app():
 
     st.write("**Project Goals, Methods, and Acknowledgements**: Explores the aims of this project, some of the methods used, and thanks those who contributed to this project.")
 
-    -st.write("**UPDATE 7/13/24**: With the depcrecation of GPT-3 models used in this 2022 experiment, the underlying models have been replaced with GPT-3.5. The custom models for BaconBot have likewise been retired and replaced.")
+    st.write("**UPDATE 7/13/24**: With the depcrecation of GPT-3 models used in this 2022 experiment, the underlying models have been replaced with GPT-3.5. The custom models for BaconBot have likewise been retired and replaced.")
